ee9360801f8f4720a44ec52a63e2bb0d9de8841f
Oziel Garza <ozielgarzalopez@gmail.com>
2013-10-08 21:22:35 -0500

    prueba

---
+++ app/Controller/ProveedoresController.php
@@ -0,0 +1,15 @@
+<?php
+class ProveedoresController extends AppController {
+	public $helpers = array('Html', 'Form');
+	public $components = array('RequestHandler');
+
+
+	//accion para desplegar el listado de proveedores
+	public function index() {
+		$proveedores = $this->Proveedor->find('all');
+		$this->set(array(
+				'proveedores' => $proveedores,
+				'_serialize' => array('proveedores')
+			));
+	}
+	
