0f0a57ce4f469e80fdb79dccbf62f4c1a7a58336
Hector <hector.hdp90@gmail.com>
2013-10-24 21:25:35 -0500

    prueba

--- app/Controller/RequestsController.php
+++ app/Controller/RequestsController.php
@@ -230,7 +230,7 @@ class RequestsController extends AppController {
 	 */
 	public function newOnlineRequest() {
 
-		if($this->request->is('ajax')) {
+		if($this->request->is('ajax') || $this->request->is('post')) {
 			$this->autoRender = false;
 			$this->Request->Content->create();
 			$content['comment'] = "hola";
