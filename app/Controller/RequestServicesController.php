70fe3d0fc19af4d9c57ae62da87ba7cf9a706694
Hector <hector.hdp90@gmail.com>
2013-11-01 17:21:05 -0600

    Envio de datos Formulario

--- app/Controller/RequestServicesController.php
+++ app/Controller/RequestServicesController.php
@@ -1,72 +1,99 @@
 <?php
 class RequestServicesController extends AppController {
-    public $uses = array('Content');
-    public $components = array('RequestHandler');
+	public $uses = array('Content, Requests');
+	public $components = array('RequestHandler');
 
-    public function beforeFilter()
-    {
-        parent::beforeFilter();
-        $this->Auth->allow('newOnlineRequest');
-    }
+	public function beforeFilter()
+	{
+		parent::beforeFilter();
+		$this->Auth->allow('newOnlineRequest');
+	}
 
-    /**
-     * newOnlineRequest method
-     *
-     * @return void
-     */
-    public function newOnlineRequest()
-    {
-            $this->response->header('Access-Control-Allow-Origin', 'http://origen.herokuapp.com');
-            $this->response->header('Access-Control-Allow-Headers ', 'Content-Type' );
-            $this->response->header('Content-Type ', 'application/json');
+	public function newOnlineRequest()
+	{
+		$this->response->header('Access-Control-Allow-Origin', 'http://origen.herokuapp.com');
+		$this->response->header('Access-Control-Allow-Headers ', 'Content-Type' );
+		$this->response->header('Content-Type ', 'application/json');
 
-            $this->autoRender = false;
-            $this->Content->create();
-            $content['comment'] = $this->request->data['comentario'];
-            $this->Content->save($content);
-            echo $content;
-    }
+		$this->autoRender = false;
 
-    /*
-    public function index() {
-        $recipes = $this->Recipe->find('all');
-        $this->set(array(
-            'recipes' => $recipes,
-            '_serialize' => array('recipes')
-        ));
-    }
+		//Empezamos trancsaccion
+		$transaction = $this->Content->getDataSource();
+		$transaction->begin();
 
-    public function view($id) {
-        $recipe = $this->Recipe->findById($id);
-        $this->set(array(
-            'recipe' => $recipe,
-            '_serialize' => array('recipe')
-        ));
-    }
+		//Tenemos que crear el "content" y guardarlo
+		$this->Content->create();
 
-    public function edit($id) {
-        $this->Recipe->id = $id;
-        if ($this->Recipe->save($this->request->data)) {
-            $message = 'Saved';
-        } else {
-            $message = 'Error';
-        }
-        $this->set(array(
-            'message' => $message,
-            '_serialize' => array('message')
-        ));
-    }
+		//Obtenemos los datos a guardar a content
+		$content['xml'] = $this->xmlCreation($this->request->data);
+		$content['comment'] = $this->request->data['comentario'];
 
-    public function delete($id) {
-        if ($this->Recipe->delete($id)) {
-            $message = 'Deleted';
-        } else {
-            $message = 'Error';
-        }
-        $this->set(array(
-            'message' => $message,
-            '_serialize' => array('message')
-        ));
-    }
-    */
+		if ($this->Content->save($content)) {
+
+			//Obteniendo los datos para crear la solicitud
+			
+			$request['category_id'] = $this->request->data['categoria'];
+			$request['content_id'] = $this->Content->getInsertID();
+
+			$this->Request->create();
+			if ($this->Request->save($request)) {
+
+				//Se ha guardado exitosamente el registro por lo tanto hacemos 
+				$transaction->commit();
+				echo "funciona";
+
+			} else{
+				$transaction->rollback();
+				echo "No funciona";
+			}
+		} else {
+			$transaction->rollback();
+			echo "No funciona";
+		}
+	}
+}
+
+	public function xmlCreation($data)
+	{
+		$xmlDoc = new DOMDocument('1.0');
+		$xmlDoc->formatOutput = true;
+		//Elemento raiz
+		$root = $xmlDoc->createElement('Request');
+		$root = $xmlDoc->appendChild($root);
+
+		//Elemento cliente
+		$customer = $xmlDoc->createElement('Customer');
+		$customer = $root->appendChild($customer);
+
+		//Elemento producto
+		$product = $xmlDoc->createElement('Product');
+		$product = $root->appendChild($product);
+
+		//Elemento comentario
+		$comments = $xmlDoc->createElement('Comments');
+		$comments = $root->appendChild($comments);  
+
+		//Iterar y agregar los campos
+		foreach ($data as $campos => $value) {
+			$tipo = substr($campos, 0, 2);
+			$campo = substr($campos, 3);
+			switch($tipo){
+			//Campo de cliente
+				case "cl":
+				$campo = $xmlDoc->createElement($campo);
+				$campo = $customer->appendChild($campo);
+				$valor = $xmlDoc->createTextNode($value);
+				$valor = $campo->appendChild($valor);
+				break;
+			//Campo de producto
+				case "pd":
+				$campo = $xmlDoc->createElement($campo);
+				$campo = $product->appendChild($campo);
+				$valor = $xmlDoc->createTextNode($value);
+				$valor = $campo->appendChild($valor);
+				break;
+			}
+		}
+		$xml =  $xmlDoc->saveXML();
+	}
 }
